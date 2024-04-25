3d3da67c76d084b1193238fc6b646eb4af39791f
Richard Connor <richard.c.connor@gmail.com>
2024-04-25 13:32:40 +0100

    added code for googlenet-places xe-tri correlations

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2024_entopy_paper
  matlab code for 2024 entropy paper
+
+Notes:
+
+get_logits folder has the code we used to generate the first 10k logits for each network from the MirFlickr 1M image collection
+
+matlab data files containing the logits are also in that folder, there is no need to regenerate using this code.
+
---
+++ fn_lib/spearman_rho.m
@@ -0,0 +1,20 @@
+function rho = spearman_rho(deltas,zetas)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% switch the data (probably outcome of pdist) to columns
+% and sort on the first column
+sorted = sortrows([ deltas' zetas' ]);
+
+% because the zeds are ordered
+zeds = (linspace(1,size(deltas,2),size(deltas,2)))';
+
+% now zedhats is the rank order of the second column
+[~,zedhats] = sortrows( sorted,2 );
+
+f1 = (zeds - zedhats);
+f2 = sum(f1 .* f1);
+t = size(deltas,2);
+
+rho =1 -  ((6 * f2) / ( t * t * t - t));
+end
